9c16596bd4a072c663c12d918272eba8bd4ab86b
Will Bradley <williambbradley@gmail.com>
2014-10-16 22:31:37 -0700

    Update install.py

--- install.py
+++ install.py
@@ -187,7 +187,7 @@ def setup_fak():
     _system('mkdir -p ~/src')
     _system('rm -rf ~/src/fak')
     os.chdir(get_home_dir_path('src'))
-    _system('git clone git@github.com:wbbradley/fak.git')
+    _system('git clone https://github.com/wbbradley/fak.git')
     fak_dir = get_home_dir_path('src/fak')
     os.chdir(fak_dir)
     _system('make')
