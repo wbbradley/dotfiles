502bb1c38f2d47d44bb00f770c0f811157b54f21
William Bradley <williambbradley@gmail.com>
2022-02-04 12:43:40 -0700

    install

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # curl https://raw.githubusercontent.com/wbbradley/dotfiles/master/install.sh | bash
-set -x
+
+die() {
+  echo "$0: error: $*" >&2
+  echo "$0: quitting..." >&2
+  exit 1
+}
 
 if [ $(uname) == 'Darwin' ]; then
     echo "Checking that homebrew is installed..."
@@ -18,37 +23,41 @@ if [ $(uname) == 'Darwin' ]; then
 fi
 
 if [ "$(uname)" == 'Linux' ]; then
+  if command -v apt 2>/dev/null; then
     sudo apt-get update
     sudo apt-get install -y exuberant-ctags stow git vim bash tmux
     sudo apt-get upgrade -y exuberant-ctags stow git vim bash tmux
+  else
+    sudo yum update -y
+    sudo yum install -y exuberant-ctags stow git vim bash tmux
+    sudo yum upgrade -y exuberant-ctags stow git vim bash tmux
+  fi
 fi
 
 # Make sure needed tools are available
-git --version > /dev/null
-curl --version > /dev/null
-vim --version > /dev/null
-stow --version > /dev/null
+git --version > /dev/null || die "git not installed"
+curl --version > /dev/null || die "curl not installed"
+vim --version > /dev/null || die "vim not installed"
+stow --version > /dev/null || die "stow not installed"
 
-cd "$HOME" || exit
+cd "$HOME" || die "no home"
 mkdir -p "$HOME/src"
-cd "$HOME/src" || exit
+cd "$HOME/src" || die "no src dir?"
 rm -rf dotfiles
-git clone git@github.com:wbbradley/dotfiles
-cd "$HOME/src/dotfiles" || exit
-git submodule init
-git submodule update
+git clone git@github.com:wbbradley/dotfiles || die "failed to clone dotfiles"
+cd "$HOME/src/dotfiles" || die "no dotfiles dir?"
 
 if [ -f "$HOME/.bashrc" ]; then
     mv "$HOME/.bashrc" "$HOME/.bashrc.bak"
 fi
 
-stow -t "$HOME" bash || exit
-stow -t "$HOME" vim || exit
+stow -t "$HOME" bash || die "failed to stow bash"
+stow -t "$HOME" vim || die "failed to stow vim"
 (
-  cd tmux || exit
+  cd tmux || die "no tmux dir in dotfiles?"
   stow -t "$HOME" "$(uname)"
-)
-stow -t "$HOME" bin
+) || die "failed to stow tmux"
+stow -t "$HOME" bin "failed to stow bin dir"
 
 # Set up my git defaults
 git config --global color.diff always
