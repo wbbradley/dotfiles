4b581c7700ec126f13a87b4f1874017ff79509ed
Will Bradley <williambbradley@gmail.com>
2022-02-24 10:09:38 -0700

    updates

--- install.sh
+++ install.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
-# curl https://raw.githubusercontent.com/wbbradley/dotfiles/master/install.sh | bash
 
 die() {
-  echo "$0: error: $*" >&2
-  echo "$0: quitting..." >&2
+  echo "$(basename "$0"): error: $*" >&2
   exit 1
 }
 
-if [ $(uname) == 'Darwin' ]; then
+BREW_DEPS=(
+  bash
+  ctags-exuberant
+  exa
+  git
+  go
+  reattach-to-user-namespace
+  ripgrep
+  swig
+  tmux
+  vim
+)
+
+if [[ $(uname) == 'Darwin' ]]; then
     echo "Checking that homebrew is installed..."
     brew --version
 
@@ -19,18 +30,18 @@ if [ $(uname) == 'Darwin' ]; then
     defaults write com.apple.finder AppleShowAllFiles true
 
     echo "Getting Exuberant-Ctags..."
-    brew install ctags-exuberant reattach-to-user-namespace go git vim bash tmux stow
+    brew install "${BREW_DEPS[@]}"
 fi
 
-if [ "$(uname)" == 'Linux' ]; then
+if [[ "$(uname)" == 'Linux' ]]; then
   if command -v apt 2>/dev/null; then
     sudo apt-get update
     sudo apt-get install -y exuberant-ctags stow git vim bash tmux
     sudo apt-get upgrade -y exuberant-ctags stow git vim bash tmux
   else
     sudo yum update -y
-    sudo yum install -y exuberant-ctags stow git vim bash tmux
-    sudo yum upgrade -y exuberant-ctags stow git vim bash tmux
+    sudo yum install -y exuberant-ctags git vim bash tmux
+    sudo yum upgrade -y exuberant-ctags git vim bash tmux
   fi
 fi
 
@@ -38,26 +49,21 @@ fi
 git --version > /dev/null || die "git not installed"
 curl --version > /dev/null || die "curl not installed"
 vim --version > /dev/null || die "vim not installed"
-stow --version > /dev/null || die "stow not installed"
 
-cd "$HOME" || die "no home"
-mkdir -p "$HOME/src"
-cd "$HOME/src" || die "no src dir?"
-rm -rf dotfiles
-git clone git@github.com:wbbradley/dotfiles || die "failed to clone dotfiles"
-cd "$HOME/src/dotfiles" || die "no dotfiles dir?"
 
-if [ -f "$HOME/.bashrc" ]; then
-    mv "$HOME/.bashrc" "$HOME/.bashrc.bak"
-fi
+dotfiles_dir="$HOME/src/dotfiles"
 
-stow -t "$HOME" bash || die "failed to stow bash"
-stow -t "$HOME" vim || die "failed to stow vim"
-(
-  cd tmux || die "no tmux dir in dotfiles?"
-  stow -t "$HOME" "$(uname)"
-) || die "failed to stow tmux"
-stow -t "$HOME" bin "failed to stow bin dir"
+for file in "$dotfiles_dir"/bash/.* "$dotfiles_dir"/vim/.* "$dotfiles_dir/tmux/$(uname)/".*; do
+  homedir_filename="$HOME/$(basename "$file")"
+  if [[ -f "$homedir_filename" ]]; then
+      mv "$homedir_filename" "$homedir_filename.bak"
+  fi
+
+  echo "linking '$homedir_filename' -> '$file'"
+  ln -sf "$file" "$homedir_filename"
+done
+
+ln -s "$dotfiles_dir/bin/bin" "$HOME/bin" || die "failed to link bin dir"
 
 # Set up my git defaults
 git config --global color.diff always
